b95c04b655b3ca408e76647605fc920a1ad55040
krishin aswani <krishinaswani@krishins-MacBook-Air.local>
2024-09-17 13:44:09 -0400

    updates to my-work

---
+++ my-work/echo-this.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Echo out something
+
+VAR1="golf"
+
+echo "Hey $LOGNAME I see that your favorite sport is $VAR1"
---
+++ my-work/read-interactive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "What is the inpput file name?: " INPUT
+read -p "What is the output file name?: " OUTPUT
+
+echo "I will use the $INPUT to give you a new $OUTPUT"
