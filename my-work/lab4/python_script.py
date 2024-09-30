e7d0feb02032b0a66e16c6b08f907a35a6a5280a
krishin aswani <krishinaswani@krishins-MacBook-Air.local>
2024-09-30 17:01:37 -0400

    lab 4 files

---
+++ my-work/lab4/python_script.py
@@ -0,0 +1,35 @@
+#!/Library/Frameworks/Python.framework/Versions/3.12/bin/python3
+
+import requests
+import boto3
+import os
+
+file_url = 'https://images.app.goo.gl/QFH4zAyBfzds1jWQ7'
+file_name = 'downloaded_file.gif'
+bucket_name = 'ds2022-vzu3vu'
+expires_in = 604800  # 7 days in seconds
+
+response = requests.get(file_url)
+if response.status_code == 200:
+    with open(file_name, 'wb') as f:
+        f.write(response.content)
+    print(f'Successfully downloaded {file_name}')
+else:
+    print('Failed to download the file')
+    exit(1)
+
+s3 = boto3.client('s3', region_name='us-east-1')
+s3.upload_file(file_name, bucket_name, file_name)
+
+presigned_url = s3.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': bucket_name, 'Key': file_name},
+    ExpiresIn=expires_in
+)
+
+print(f'Presigned URL (expires in 7 days): {presigned_url}')
+
+os.remove(file_name)
+
+# 
+https://ds2022-vzu3vu.s3.amazonaws.com/downloaded_file.gif?AWSAccessKeyId=AKIAX2DZEIRCDTQKYXN7&Signature=lUQ%2Fn0UIQlT2cd1puFkBcUoNlOQ%3D&Expires=1728334494
---
+++ my-work/lab4/upload_and_presign.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define variables
+FILE_NAME="realmadrid.png"
+BUCKET_NAME="ds2022-vzu3vu"
+
+# Upload the file to the S3 bucket
+aws s3 cp "$FILE_NAME" "s3://$BUCKET_NAME/"
+
+# Check if the upload was successful
+if [ $? -eq 0 ]; then
+    echo "File uploaded successfully."
+
+    # Generate a presigned URL for the uploaded file
+    PRESIGNED_URL=$(aws s3 presign s3://ds2022-vzu3vu/realmadrid.png --expires-in 604800)
+
+    echo "Presigned URL (expires in 7 days): $PRESIGNED_URL"
+else
+    echo "Failed to upload the file."
+fi
+
+# https://ds2022-vzu3vu.s3.us-east-1.amazonaws.com/realmadrid.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAX2DZEIRCDTQKYXN7%2F20240930%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240930T153846Z&X-Amz-Expires=30&X-Amz-SignedHeaders=host&X-Amz-Signature=32497c5fd2a3bf46b7e074d4f41c66464e693a52ac11ce6e2324c20657a32549
